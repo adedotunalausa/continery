e5f5974da1086dc93115813bac99f5c5cc61e416
Adedotun Alausa <Adedotun@Adedotuns-MacBook-Pro.local>
2020-08-06 00:54:22 +0100

    Updates

---
+++ precache-manifest.077e4ae77555b7a0de211855a59bba38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a899aa3ce63bcfd93bcb003ec5a54a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a33144fecbb6126e6c",
+    "url": "/static/js/2.da3a257c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.da3a257c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d680e551b204b511904a",
+    "url": "/static/js/main.2740fb35.chunk.js"
+  },
+  {
+    "revision": "c825a6d8c5ac40e9f221",
+    "url": "/static/js/runtime-main.824d5f8a.js"
+  }
+]);

98a095e57a025bbc3373af1cc33b61bf88fa912c
Adedotun Alausa <Adedotun@Adedotuns-MacBook-Pro.local>
2020-08-12 15:42:30 +0100

    Remove theme sign for later

--- src/components/ThemeBtn.jsx
+++ src/components/ThemeBtn.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 export const ThemeBtn = (props) => {
     return (
         <div className="theme-button">
-            <div className="theme-sign">
-                {props.mode === "dark-theme" ? <i className="fas fa-moon"></i> : <i className="fas fa-sun"></i>}
-            </div>
             <input id="dark-theme" onClick={props.addTheme} className="theme-input" type="checkbox" />
             <label htmlFor="dark-theme">Theme</label>
         </div>

1c4e73d585fdf54a121c0424b1f1839a6c7772b4
Adedotun Alausa <Adedotun@Adedotuns-MacBook-Pro.local>
2020-08-13 11:43:46 +0100

    Fix the details page

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -20,46 +20,48 @@ function CountryDetails({ match }) {
 
     return (
         <div className="details-container">
-            <div className="back-btn">
-                <Link className="link" to="/countries">
-                    <ArrowBackIcon className="back-arrow" fontSize="small" />
-                    <p>Back</p>
-                </Link>
-            </div>
-            <div>
-                {countryDetails.map((data) => {
-                    return (
-                        <div className="details-container-inner">
-                            <div className="details-flag">
-                                <img src={data.flag} alt="flag-img" />
-                            </div>
-                            <div className="details-info">
-                                <h1>{data.name}</h1>
-                                <div className="details-info-inner">
-                                    <div className="details-info-1">
-                                        <p><b>Native Name:</b> {data.nativeName}</p>
-                                        <p><b>Population:</b> {data.population}</p>
-                                        <p><b>Region:</b> {data.region}</p>
-                                        <p><b> Sub Region:</b> {data.subregion}</p>
-                                        <p><b>Capital:</b> {data.capital}</p>
+            <div className="details-content">
+                <div className="back-btn">
+                    <Link className="link" to="/countries">
+                        <ArrowBackIcon className="back-arrow" fontSize="small" />
+                        <p>Back</p>
+                    </Link>
+                </div>
+                <div>
+                    {countryDetails.map((data) => {
+                        return (
+                            <div className="details-container-inner">
+                                <div className="details-flag">
+                                    <img src={data.flag} alt="flag-img" />
+                                </div>
+                                <div className="details-info">
+                                    <h1>{data.name}</h1>
+                                    <div className="details-info-inner">
+                                        <div className="details-info-1">
+                                            <p><b>Native Name:</b> {data.nativeName}</p>
+                                            <p><b>Population:</b> {data.population}</p>
+                                            <p><b>Region:</b> {data.region}</p>
+                                            <p><b> Sub Region:</b> {data.subregion}</p>
+                                            <p><b>Capital:</b> {data.capital}</p>
+                                        </div>
+                                        <div className="details-info-2">
+                                            <p><b>Top Level Domain:</b> {data.topLevelDomain}</p>
+                                            <p><b>Currencies:</b> {data.currencies.map(data => <span>{data.name}</span>)}</p>
+                                            <p><b>Languages:</b> {data.languages.map(data => <span>{data.name}, </span>)}</p>
+                                        </div>
                                     </div>
-                                    <div className="details-info-2">
-                                        <p><b>Top Level Domain:</b> {data.topLevelDomain}</p>
-                                        <p><b>Currencies:</b> {data.currencies.map(data => <span>{data.name}</span>)}</p>
-                                        <p><b>Languages:</b> {data.languages.map(data => <span>{data.name}, </span>)}</p>
+                                    <div className="border-countries">
+                                        <h4><b>Border Countries:</b></h4>
+                                        <div>
+                                            {data.borders.map(data => <p>{data} </p>)}
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="border-countries">
-                                    <h4><b>Border Countries:</b></h4>
-                                    <div>
-                                        {data.borders.map(data => <p>{data} </p>)}
-                                    </div>
-                                </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
 
+                </div>
             </div>
         </div>
     )
